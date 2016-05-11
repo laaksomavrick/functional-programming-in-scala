59b1901b38730ba8d8d9e866f971df1891f602d3
mav <laakso.mavrick@gmail.com>
2016-05-10 21:59:59 -0400

    ch 2 done

--- ch2/abs.scala
+++ ch2/abs.scala
@@ -18,6 +18,14 @@ object abs {
             if (n <= 1) n 
             else fib(n - 1) + fib(n - 2)
 
+    def partial1[A,B,C](a: A, f: (A, B) => C): B => C =
+        b => f(a, b)
+
+    def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+        a => (b => f(a, b))
+    
+    def compose[A, B, C](f: B => C, g: A => B): A => C = 
+        a => f(g(a))
 
      def formatResult(name: String, n: Int, f: Int => Int) = {
 
